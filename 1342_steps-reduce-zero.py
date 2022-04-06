3cf78c65f0602c889b8b79a0526c357e776e5dac
mazin-abdelghany <68043318+mazin-abdelghany@users.noreply.github.com>
2022-04-05 19:32:47 -0700

    Remove redundant counter addition

--- 1342_steps-reduce-zero.py
+++ 1342_steps-reduce-zero.py
@@ -3,8 +3,7 @@ def numberOfSteps(num):
     while num > 0:
         if num % 2 == 0:
             num = num / 2
-            counter += 1
         else:
             num = num - 1
-            counter += 1
+        counter += 1
     return counter
